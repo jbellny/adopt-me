47630af0d0bcaf441887d8f16c28b3451ae66adc
jbellny <62436650+jbellny@users.noreply.github.com>
2024-06-10 16:30:52 -0400

    implement react-router and react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import { createRoot } from "react-dom";
-import Pet from "./Pet";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity, // 5 minutes
+      cacheTime: Infinity, // 24 hours
+    },
+  },
+});
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Rafiki" animal="Cat" breed="Domestic Short Hair" />
-    </div>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <div>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </div>
+      </QueryClientProvider>
+    </BrowserRouter>
   );
 };
 
---
+++ src/Results.jsx
@@ -0,0 +1,25 @@
+import Pet from "./Pet";
+
+const Results = ({ pets }) => {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h2>No Pets Found</h2>
+      ) : (
+        pets.map((pet) => (
+          <Pet
+            animal={pet.animal}
+            key={pet.id}
+            id={pet.id}
+            name={pet.name}
+            breed={pet.breed}
+            images={pet.images}
+            location={`${pet.city}, ${pet.state}`}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Results;
